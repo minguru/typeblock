32ea539c5b06232113935dccdfc57ca74526ec69
박민규 <bagmingyu@bagmingyuui-MacBookAir-2.local>
2023-08-09 20:57:50 +0900

    click event and small style update

--- src/js/kbFunc.js
+++ src/js/kbFunc.js
@@ -1,22 +1,52 @@
 // keyboard reacting function scripts
 
 const { $keyboard, $main, kbLayout } = require('./kbGen')
+const { $tbTextarea } = require('./tbGen')
 
-function keyPress(e, t) {
+const keys = $keyboard.childNodes
+
+function keyPress(e, t, k) {
+    // e = eventType
     // t = type : pressed or released
-    const pkey = e.code
-    const keys = $keyboard.childNodes
+    // k = key elements
+    $tbTextarea.focus()
+
+    let evtType = e.type
+    let pkey
+
+    if (evtType === 'keydown' || 'keyup') pkey = e.code
+    if (evtType === 'mousedown' || 'mouseup') pkey = e.target.getAttribute('data-keyCode')
     
-    keys.forEach((key, idx) => {
+    k.forEach((key, idx) => {
         const kcode = key.getAttribute('data-keyCode')
         if (pkey == kcode) t ? key.classList.add('p') : key.classList.remove('p')
     })
+
+    if (evtType === 'mousedown') {
+        // typing text on textarea
+        const keyValue = e.target.getAttribute('data-keyname')
+        if (!(keyValue.length > 1)) $tbTextarea.value = `${$tbTextarea.value}${keyValue.toString()}`
+        else if (keyValue == 'spacebar') $tbTextarea.value = `${$tbTextarea.value} `
+        else if (keyValue == 'backspace') $tbTextarea.value = `${$tbTextarea.value.substring(0, $tbTextarea.value.length - 1)}`
+    }
 }
 
 window.addEventListener('keydown', (e) => {
-    keyPress(e, true)
+    keyPress(e, true, keys)
 })
 
 window.addEventListener('keyup', (e) => {
-    keyPress(e, false)
+    keyPress(e, false, keys)
+})
+
+keys.forEach((key, idx) => {
+    key.addEventListener('mousedown', (e) => {
+        keyPress(e, true, keys)
+    })
+    key.addEventListener('mouseup', (e) => {
+        keyPress(e, false, keys)
+    })
+    key.addEventListener('mouseout', (e) => {
+        keyPress(e, false, keys)
+    })
 })
