db89c3014820a050198e9c5111a84e601674d72c
박민규 <bagmingyu@bagmingyuui-MacBookAir-2.local>
2023-08-23 20:23:16 +0900

    fixed the visual keyboard press reacting

--- src/js/kbFunc.js
+++ src/js/kbFunc.js
@@ -3,50 +3,101 @@
 const { $keyboard, $main, kbLayout } = require('./kbGen')
 const { $tbTextarea } = require('./tbGen')
 
-const keys = $keyboard.childNodes
+const keys = $keyboard.querySelectorAll('key')
 
-function keyPress(e, t, k) {
-    // e = eventType
-    // t = type : pressed or released
-    // k = key elements
-    $tbTextarea.focus()
+/**
+ * @description graphic keyboard pressing visualizer
+ * @param {Event} evt 
+ * @param {Boolean} state
+*/
+let capslock = false
+const keyPress = (evt, state) => {
+    const e = evt,
+          t = evt.type
+          tg = t === 'mousedown' ? evt.target : t === 'keydown' ? evt.code : null,
+          s = state
 
-    let evtType = e.type
-    let pkey
+    if (s) {
+        // press
+        if (t === 'keydown') {
+            keys.forEach((key, idx) => {
+                tg === key.getAttribute('data-keycode') ? key.classList.add('p') : false
+            })
+        }
+        if (t === 'mousedown') {
+            tg.classList.add('p')
 
-    if (evtType === 'keydown' || 'keyup') pkey = e.code
-    if (evtType === 'mousedown' || 'mouseup') pkey = e.target.getAttribute('data-keyCode')
-    
-    k.forEach((key, idx) => {
-        const kcode = key.getAttribute('data-keyCode')
-        if (pkey == kcode) t ? key.classList.add('p') : key.classList.remove('p')
-    })
-
-    if (evtType === 'mousedown') {
-        // typing text on textarea
-        const keyValue = e.target.getAttribute('data-keyname')
-        if (!(keyValue.length > 1)) $tbTextarea.value = `${$tbTextarea.value}${keyValue.toString()}`
-        else if (keyValue == 'spacebar') $tbTextarea.value = `${$tbTextarea.value} `
-        else if (keyValue == 'backspace') $tbTextarea.value = `${$tbTextarea.value.substring(0, $tbTextarea.value.length - 1)}`
+            const keyData = tg.getAttribute('data-keydata')
+            switch(keyData) {
+                case 'backspace':
+                    $tbTextarea.value = `${$tbTextarea.value.substr(0, $tbTextarea.value.length - 1)}`
+                    break
+                case 'lsb':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? '{' : '['}`
+                    break
+                case 'rsb':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? '}' : ']'}`
+                    break
+                case 'backdash':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? '|' : '\\'}`
+                    break
+                case 'minus':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? '_' : '-'}`
+                    break
+                case 'equal':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? '+' : '='}`
+                    break
+                case 'colon':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? ':' : ';'}`
+                    break
+                case 'quote':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? '"' : '\''}`
+                    break
+                case 'lt':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? '<' : ','}`
+                    break
+                case 'gt':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? '>' : '.'}`
+                    break
+                case 'dash':
+                    $tbTextarea.value = `${$tbTextarea.value}${capslock ? '?' : '/'}`
+                    break
+                case 'capslock':
+                    if (capslock) {
+                        capslock = false
+                        tg.classList.remove('active')
+                    } else {
+                        capslock = true
+                        tg.classList.add('active')
+                    }
+                    break;
+                case 'spacebar':
+                    $tbTextarea.value = `${$tbTextarea.value} `
+                    break
+                default:
+                    keyData.length <= 1 ? $tbTextarea.value = `${$tbTextarea.value}${capslock ? keyData.toUpperCase() : keyData.toLowerCase()}` : false
+                    break
+            }
+        }
+    } else {
+        // release press
+        keys.forEach((key, idx) => { key.classList.remove('p') })
     }
 }
 
-window.addEventListener('keydown', (e) => {
-    keyPress(e, true, keys)
+window.addEventListener('keydown', e => {
+    $tbTextarea.focus()
+    keyPress(e, true)
 })
-
-window.addEventListener('keyup', (e) => {
-    keyPress(e, false, keys)
+window.addEventListener('keyup', e => {
+    keyPress(e, false)
 })
 
 keys.forEach((key, idx) => {
-    key.addEventListener('mousedown', (e) => {
-        keyPress(e, true, keys)
+    key.addEventListener('mousedown', e => {
+        keyPress(e, true)
     })
-    key.addEventListener('mouseup', (e) => {
-        keyPress(e, false, keys)
-    })
-    key.addEventListener('mouseout', (e) => {
-        keyPress(e, false, keys)
+    key.addEventListener('mouseup', e => {
+        keyPress(e, false)
     })
 })
--- src/js/kbGen.js
+++ src/js/kbGen.js
@@ -2,19 +2,19 @@
 
 const kbLayout = {
     R4: [
-        {name: 'tilde', unit: 'u1', code: 'Backquote'}, {name: '1', unit: 'u1', code: 'Digit1'}, {name: '2', unit: 'u1', code: 'Digit2'}, {name: '3', unit: 'u1', code: 'Digit3'}, {name: '4', unit: 'u1', code: 'Digit4'}, {name: '5', unit: 'u1', code: 'Digit5'}, {name: '6', unit: 'u1', code: 'Digit6'}, {name: '7', unit: 'u1', code: 'Digit7'}, {name: '8', unit: 'u1', code: 'Digit8'}, {name: '9', unit: 'u1', code: 'Digit9'}, {name: '0', unit: 'u1', code: 'Digit0'}, {name: 'minus', unit: 'u1', code: 'Minus'}, {name: 'equal', unit: 'u1', code: 'Equal'}, {name: 'backspace', unit: 'u1h', code: 'Backspace'} // 14
+        {data: 'tilde', unit: 'u1', code: 'Backquote'}, {data: '1', unit: 'u1', code: 'Digit1'}, {data: '2', unit: 'u1', code: 'Digit2'}, {data: '3', unit: 'u1', code: 'Digit3'}, {data: '4', unit: 'u1', code: 'Digit4'}, {data: '5', unit: 'u1', code: 'Digit5'}, {data: '6', unit: 'u1', code: 'Digit6'}, {data: '7', unit: 'u1', code: 'Digit7'}, {data: '8', unit: 'u1', code: 'Digit8'}, {data: '9', unit: 'u1', code: 'Digit9'}, {data: '0', unit: 'u1', code: 'Digit0'}, {data: 'minus', unit: 'u1', code: 'Minus'}, {data: 'equal', unit: 'u1', code: 'Equal'}, {data: 'backspace', unit: 'u1h', code: 'Backspace'} // 14
     ],
     R3: [
-        {name: 'tab', unit: 'u1h', code: 'Tab'}, {name: 'q', unit: 'u1', code: 'KeyQ'}, {name: 'w', unit: 'u1', code: 'KeyW'}, {name: 'e', unit: 'u1', code: 'KeyE'}, {name: 'r', unit: 'u1', code: 'KeyR'}, {name: 't', unit: 'u1', code: 'KeyT'}, {name: 'y', unit: 'u1', code: 'KeyY'}, {name: 'u', unit: 'u1', code: 'KeyU'}, {name: 'i', unit: 'u1', code: 'KeyI'}, {name: 'o', unit: 'u1', code: 'KeyO'}, {name: 'p', unit: 'u1', code: 'KeyP'}, {name: 'lsb', unit: 'u1', code: 'BracketLeft'}, {name: 'rsb', unit: 'u1', code: 'BracketRight'}, {name: 'backdash', unit: 'u1', code: 'Backslash'} // 14
+        {data: 'tab', unit: 'u1h', code: 'Tab'}, {data: 'q', unit: 'u1', code: 'KeyQ'}, {data: 'w', unit: 'u1', code: 'KeyW'}, {data: 'e', unit: 'u1', code: 'KeyE'}, {data: 'r', unit: 'u1', code: 'KeyR'}, {data: 't', unit: 'u1', code: 'KeyT'}, {data: 'y', unit: 'u1', code: 'KeyY'}, {data: 'u', unit: 'u1', code: 'KeyU'}, {data: 'i', unit: 'u1', code: 'KeyI'}, {data: 'o', unit: 'u1', code: 'KeyO'}, {data: 'p', unit: 'u1', code: 'KeyP'}, {data: 'lsb', unit: 'u1', code: 'BracketLeft'}, {data: 'rsb', unit: 'u1', code: 'BracketRight'}, {data: 'backdash', unit: 'u1', code: 'Backslash'} // 14
     ],
     R2: [
-        {name: 'capslock', unit: 'u1tq', code: 'CapsLock'}, {name: 'a', unit: 'u1', code: 'KeyA'}, {name: 's', unit: 'u1', code: 'KeyS'}, {name: 'd', unit: 'u1', code: 'KeyD'}, {name: 'f', unit: 'u1', code: 'KeyF'}, {name: 'g', unit: 'u1', code: 'KeyG'}, {name: 'h', unit: 'u1', code: 'KeyH'}, {name: 'j', unit: 'u1', code: 'KeyJ'}, {name: 'k', unit: 'u1', code: 'KeyK'}, {name: 'l', unit: 'u1', code: 'KeyL'}, {name: 'colon', unit: 'u1', code: 'Semicolon'}, {name: 'quote', unit: 'u1', code: 'Quote'}, {name: 'enter', unit: 'u1tq', code: 'Enter'} // 13
+        {data: 'capslock', unit: 'u1tq', code: 'CapsLock'}, {data: 'a', unit: 'u1', code: 'KeyA'}, {data: 's', unit: 'u1', code: 'KeyS'}, {data: 'd', unit: 'u1', code: 'KeyD'}, {data: 'f', unit: 'u1', code: 'KeyF'}, {data: 'g', unit: 'u1', code: 'KeyG'}, {data: 'h', unit: 'u1', code: 'KeyH'}, {data: 'j', unit: 'u1', code: 'KeyJ'}, {data: 'k', unit: 'u1', code: 'KeyK'}, {data: 'l', unit: 'u1', code: 'KeyL'}, {data: 'colon', unit: 'u1', code: 'Semicolon'}, {data: 'quote', unit: 'u1', code: 'Quote'}, {data: 'enter', unit: 'u1tq', code: 'Enter'} // 13
     ],
     R1: [
-        {name: 'lshift', unit: 'u2q', code: 'ShiftLeft'}, {name: 'z', unit: 'u1', code: 'KeyZ'}, {name: 'x', unit: 'u1', code: 'KeyX'}, {name: 'c', unit: 'u1', code: 'KeyC'}, {name: 'v', unit: 'u1', code: 'KeyV'}, {name: 'b', unit: 'u1', code: 'KeyB'}, {name: 'n', unit: 'u1', code: 'KeyN'}, {name: 'm', unit: 'u1', code: 'KeyM'}, {name: 'lt', unit: 'u1', code: 'Comma'}, {name: 'gt', unit: 'u1', code: 'Period'}, {name: 'dash', unit: 'u1', code: 'Slash'}, {name: 'rshift', unit: 'u2q', code: 'ShiftRight'} // 12
+        {data: 'lshift', unit: 'u2q', code: 'ShiftLeft'}, {data: 'z', unit: 'u1', code: 'KeyZ'}, {data: 'x', unit: 'u1', code: 'KeyX'}, {data: 'c', unit: 'u1', code: 'KeyC'}, {data: 'v', unit: 'u1', code: 'KeyV'}, {data: 'b', unit: 'u1', code: 'KeyB'}, {data: 'n', unit: 'u1', code: 'KeyN'}, {data: 'm', unit: 'u1', code: 'KeyM'}, {data: 'lt', unit: 'u1', code: 'Comma'}, {data: 'gt', unit: 'u1', code: 'Period'}, {data: 'dash', unit: 'u1', code: 'Slash'}, {data: 'rshift', unit: 'u2q', code: 'ShiftRight'} // 12
     ],
     bR1: [
-        {name: 'lctrl', unit: 'u1h', code: 'ControlLeft'}, {name: 'lopt', unit: 'u1h', code: 'AltLeft'}, {name: 'lcmnd', unit: 'u1tq', code: 'MetaLeft'}, {name: 'spacebar', unit: 'u5', code: 'Space'}, {name: 'rcmnd', unit: 'u1tq', code: 'MetaRight'}, {name: 'ropt', unit: 'u1h', code: 'AltRight'}, {name: 'rctrl', unit: 'u1h', code: 'ControlRight'} // 7
+        {data: 'lctrl', unit: 'u1h', code: 'ControlLeft'}, {data: 'lopt', unit: 'u1h', code: 'AltLeft'}, {data: 'lcmnd', unit: 'u1tq', code: 'MetaLeft'}, {data: 'spacebar', unit: 'u5', code: 'Space'}, {data: 'rcmnd', unit: 'u1tq', code: 'MetaRight'}, {data: 'ropt', unit: 'u1h', code: 'AltRight'}, {data: 'rctrl', unit: 'u1h', code: 'ControlRight'} // 7
     ]
 }
 
@@ -28,8 +28,8 @@ for (rown in kbLayout) {
     for (let i = 0; i < kbLayout[rown].length; i++) {
         const key = document.createElement('key')
         key.className = `${rown} ${kbLayout[rown][i].unit}`
-        key.setAttribute('data-keyName', kbLayout[rown][i].name)
-        key.setAttribute('data-keyCode', kbLayout[rown][i].code)
+        key.setAttribute('data-keydata', kbLayout[rown][i].data)
+        key.setAttribute('data-keycode', kbLayout[rown][i].code)
         keyboard.appendChild(key)
     }
 }
